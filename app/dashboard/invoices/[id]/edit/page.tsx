7da4ed5ba0fa625cac327c5bf314158957958274
DewinU <umana.dewin@gmail.com>
2023-10-28 22:32:21 -0600

    Crud operations for invoices

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -7,6 +7,8 @@ export default async function Page({ params }: {params: {id: string}}) {
     const invoice = await fetchInvoiceById(id);
     const customers = await fetchCustomers();
 
+    if(!invoice) return <div>Invoice not found</div>
+
   return (
     <main>
       <Breadcrumbs
